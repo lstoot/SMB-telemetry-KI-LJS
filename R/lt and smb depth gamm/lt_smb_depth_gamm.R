260d3fd581d1ed19187e7c25d545639188a11828
benjaminhlina <benjamin.hlina@gmail.com>
2023-11-15 18:03:43 -0500

    updated autocorelation and updated model

--- R/lt and smb depth gamm/lt_smb_depth_gamm.R
+++ R/lt and smb depth gamm/lt_smb_depth_gamm.R
@@ -18,7 +18,7 @@ dat <- read_rds(here("Data",
                      "daily_smb_depth_data.rds"))
 
 
-glimpse(data)
+glimpse(dat)
 # ---- look at the distribution ---- 
 
 ggplot(data = dat, aes(x = mean_value)) + 
@@ -27,40 +27,33 @@ ggplot(data = dat, aes(x = mean_value)) +
 
 # ---- create gam models -----
 
-m <- bam(mean_value ~ species +
-           ti(doy, fish_basin, by = species, bs = c("cc", "fs"), 
+m <- bam(mean_value ~ fish_basin +
+           ti(doy, fish_basin, bs = c("cc", "fs"), 
              k = c(15, 3)) + 
-           s(doy, by = species, k = 15, bs = "cc") +
-           s(floy_tag, year, bs = c("re", "re")),
+           s(doy, by = fish_basin, k = 15, bs = "cc") +
+           s(floy_tag, bs = "re") + 
+           s(year, bs = "re"),
          method = "fREML", 
          data = dat, 
          family = Gamma(link = "log"), 
          select = TRUE)
 
-r1 <- start_value_rho(m, plot = TRUE)
+r1 <- start_value_rho(m, plot = TRUE, lag = 21)
 
 r1
-m1 <- bam(
-  mean_value ~ species +
-    ti(doy, fish_basin, by = species, bs = c("cc", "fs"), 
-       k = c(15, 3)) + 
-    s(doy, by = species, k = 15, bs = "cc") +
-    s(floy_tag, year, bs = c("re", "re")),
-  method = "fREML", 
-  data = dat, 
-  family = Gamma(link = "log"), 
-  select = TRUE, 
+m1 <- update(m,
   discrete = TRUE, 
   rho = r1, 
   AR.start = start_event
 )
 
-gam.check(m)
+
+# gam.check(m)
 gam.check(m1)
 appraise(m)
 appraise(m1)
 
 write_rds(m1, here("model objects", 
-                   "lt_smb_gamm_depth.rds"))
+                   "smb_gamm_depth.rds"))
 
 
